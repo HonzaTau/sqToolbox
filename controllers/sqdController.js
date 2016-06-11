9cc781b77c8706478651409ead574a9d6a9ca276
Jan Tauwinkl <Jan Tauwinkl>
2016-06-11 21:50:19 +0200

    Add another project and display some meaningful metrics

--- controllers/sqdController.js
+++ controllers/sqdController.js
@@ -5,6 +5,10 @@
 			projectKey: "NCentral_AgentWindows_CurrentVersion",
 			baselineDate: "2016-02-13T13:02:59+0100",
 		},
+		{
+			projectKey: "NCentral_AgentWindows_SprintDSRCRSMRemoval",
+			baselineDate: "2016-02-13T13:02:59+0100",
+		},
 	],
 };
 
@@ -12,14 +16,43 @@ var app = angular.module("SonarQubeDashboardApp", ['ngCookies']);
 
 app.controller("sqdController", function ($scope, $http, $cookies) {
 
-	$scope.getMetrics = function getMetrics() {
+	$scope.getMetrics = function() {
 		$scope.projects = [];
-		$http.get(sqdConfiguration.sonarQubeUrl + "/api/timemachine/index?resource=" + sqdConfiguration.projects[0].projectKey + "&metrics=duplicated_blocks,duplicated_lines,duplicated_lines_density,coverage,line_coverage,function_complexity,file_complexity,class_complexity&fromDateTime=" + sqdConfiguration.projects[0].baselineDate)
+		for (projectIndex = 0; projectIndex < sqdConfiguration.projects.length; projectIndex++) { 
+			$scope.getMetricsForProject(projectIndex);
+		}
+	}
+
+	$scope.getMetricsForProject = function(projectIndex) {
+		var apiUrl = sqdConfiguration.sonarQubeUrl + "/api/timemachine/index?&metrics=duplicated_blocks,duplicated_lines,duplicated_lines_density,coverage,line_coverage,function_complexity,file_complexity,class_complexity";
+		apiUrl += "&resource=" + sqdConfiguration.projects[projectIndex].projectKey;
+		apiUrl += "&fromDateTime=" + sqdConfiguration.projects[projectIndex].baselineDate;
+		
+		$http.get(apiUrl)
 			.success(function (response, status) {
-				var project = {
-					coverage: response[0].cells[0].v[1],
+				var coverage_index =
+					duplicated_lines_density_index =
+					function_complexity_index = 0;
+				for (colIndex = 0; colIndex < response[0].cols.length; colIndex++) {
+					switch (response[0].cols[colIndex].metric) {
+						case "coverage":
+							coverage_index = colIndex;
+							break;
+						case "duplicated_lines_density":
+							duplicated_lines_density_index = colIndex;
+							break;
+						case "function_complexity":
+							function_complexity_index = colIndex;
+							break;
+					}
 				};
-				$scope.projects.push(project);
+				var lastCell = response[0].cells[response[0].cells.length - 1];
+				var project = {
+					coverage: lastCell.v[coverage_index],
+					duplicated_lines_density: lastCell.v[duplicated_lines_density_index],
+					function_complexity: lastCell.v[function_complexity_index],
+				};
+				$scope.projects[projectIndex] = project;
 			})
 			.error(function (response, status) {
 				alert('aaa');
