668f4a4d5036910df47647d00390defb9e7ce912
Jan Tauwinkl <Jan Tauwinkl>
2016-06-12 13:16:56 +0200

    Added number of new issues

--- controllers/sqdController.js
+++ controllers/sqdController.js
@@ -3,11 +3,13 @@
 	projects: [
 		{
 			projectKey: "NCentral_AgentWindows_CurrentVersion",
-			baselineDate: "2016-02-13T13:02:59+0100",
+			baselineDate: "2016-02-13T13:02:59%2b0100",
+			name: "dev-10.3",
 		},
 		{
 			projectKey: "NCentral_AgentWindows_SprintDSRCRSMRemoval",
-			baselineDate: "2016-02-13T13:02:59+0100",
+			baselineDate: "2016-02-13T13:02:59%2b0100",
+			name: "DSRC Removal",
 		},
 	],
 };
@@ -19,16 +21,17 @@ app.controller("sqdController", function ($scope, $http, $cookies) {
 	$scope.getMetrics = function() {
 		$scope.projects = [];
 		for (projectIndex = 0; projectIndex < sqdConfiguration.projects.length; projectIndex++) { 
+			$scope.projects[projectIndex] = { name: sqdConfiguration.projects[projectIndex].name, };
 			$scope.getMetricsForProject(projectIndex);
 		}
 	}
 
 	$scope.getMetricsForProject = function(projectIndex) {
-		var apiUrl = sqdConfiguration.sonarQubeUrl + "/api/timemachine/index?&metrics=duplicated_blocks,duplicated_lines,duplicated_lines_density,coverage,line_coverage,function_complexity,file_complexity,class_complexity";
-		apiUrl += "&resource=" + sqdConfiguration.projects[projectIndex].projectKey;
-		apiUrl += "&fromDateTime=" + sqdConfiguration.projects[projectIndex].baselineDate;
+		var timemachineApiUrl = sqdConfiguration.sonarQubeUrl + "/api/timemachine/index?&metrics=duplicated_blocks,duplicated_lines,duplicated_lines_density,coverage,line_coverage,function_complexity,file_complexity,class_complexity";
+		timemachineApiUrl += "&resource=" + sqdConfiguration.projects[projectIndex].projectKey;
+		timemachineApiUrl += "&fromDateTime=" + sqdConfiguration.projects[projectIndex].baselineDate;
 		
-		$http.get(apiUrl)
+		$http.get(timemachineApiUrl)
 			.success(function (response, status) {
 				var coverage_index =
 					duplicated_lines_density_index =
@@ -46,17 +49,48 @@ app.controller("sqdController", function ($scope, $http, $cookies) {
 							break;
 					}
 				};
+				var firstCell = response[0].cells[0];
 				var lastCell = response[0].cells[response[0].cells.length - 1];
-				var project = {
-					coverage: lastCell.v[coverage_index],
-					duplicated_lines_density: lastCell.v[duplicated_lines_density_index],
-					function_complexity: lastCell.v[function_complexity_index],
-				};
-				$scope.projects[projectIndex] = project;
+				
+				$scope.projects[projectIndex].coverage = lastCell.v[coverage_index];
+				$scope.projects[projectIndex].coverage_diff = lastCell.v[coverage_index] - firstCell.v[coverage_index];
+				$scope.projects[projectIndex].function_complexity = lastCell.v[function_complexity_index];
+				$scope.projects[projectIndex].function_complexity_diff = lastCell.v[function_complexity_index] - firstCell.v[function_complexity_index];
+				$scope.projects[projectIndex].duplicated_lines_density = lastCell.v[duplicated_lines_density_index];
+				$scope.projects[projectIndex].duplicated_lines_density_diff = lastCell.v[duplicated_lines_density_index] - firstCell.v[duplicated_lines_density_index];
 			})
 			.error(function (response, status) {
-				alert('aaa');
+				alert('Error when getting metrics using ' + timemachineApiUrl);
 			});
+			
+		var issuesApiUrl = sqdConfiguration.sonarQubeUrl + "/api/issues/search?resolved=false&ps=1";
+		issuesApiUrl += "&projectKeys=" + sqdConfiguration.projects[projectIndex].projectKey;
+		issuesApiUrl += "&createdAfter=" + sqdConfiguration.projects[projectIndex].baselineDate;
+		
+		$http.get(issuesApiUrl + "&severities=BLOCKER")
+			.success(function (response, status) {
+				$scope.projects[projectIndex].blocker_issues = response.total;
+			})
+			.error(function (response, status) {
+				alert('Error when getting metrics using ' + issuesApiUrl);
+			});
+		
+		$http.get(issuesApiUrl + "&severities=CRITICAL")
+			.success(function (response, status) {
+				$scope.projects[projectIndex].critical_issues = response.total;
+			})
+			.error(function (response, status) {
+				alert('Error when getting metrics using ' + issuesApiUrl);
+			});
+		
+		$http.get(issuesApiUrl + "&severities=INFO,MINOR,MAJOR")
+			.success(function (response, status) {
+				$scope.projects[projectIndex].minor_issues = response.total;
+			})
+			.error(function (response, status) {
+				alert('Error when getting metrics using ' + issuesApiUrl);
+			});
+		
 	}
 
 	$scope.getMetrics();
